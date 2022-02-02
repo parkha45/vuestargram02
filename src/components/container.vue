16ec2a8b4ba413e81086a9c4120cb1ed7c3e38d5
haemory <parkha45@naver.com>
2022-02-02 16:58:33 +0900

    first layout

---
+++ src/components/container.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+		<Post v-for="(user, i) in UserData" :key="i" :user="user" />
+  </div>
+</template>
+
+<script>
+import Post from './post.vue'
+
+export default {
+	name: 'container',
+	components: {
+		Post: Post
+	},
+	props: {
+		UserData: Array,
+	}
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/post.vue
@@ -0,0 +1,62 @@
+<template>
+	<div class="post">
+		<div class="post-header">
+			<div class="profile" :style="{ backgroundImage: `url(${ user.userImage })` }" ></div>
+			<span class="profile-name">{{ user.name }}</span>
+		</div>
+		<div class="post-body" :style="{ backgroundImage:`url(${ user.postImage })` }"></div>
+		<div class="post-content">
+			<p>{{ user.likes }} Likes</p>
+			<p><strong>{{ user.name }}</strong> {{ user.content }}</p>
+			<p class="date">{{ user.date }}</p>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'post',
+	props: {
+		user: Object,
+	}
+}
+</script>
+
+<style>
+	.post {
+		width: 100%;
+	}
+	.profile {
+		width: 30px;
+		height: 30px;
+		background-size: 100%;
+		border-radius: 50%;
+		float: left;
+	}
+	.profile-name {
+		display: block;
+		float: left;
+		padding-left: 10px;
+		padding-top: 7px;
+		font-size: 14px;
+	}
+	.post-header {
+		height: 30px;
+		padding: 10px;
+	}
+	.post-body {
+		height: 450px;
+		background-position: center;
+		background-size: cover;
+	}
+	.post-content {
+		padding-left: 15px;
+		padding-right: 15px;
+		font-size: 14px;
+	}
+	.date {
+		font-size: 11px;
+		color: grey;
+		margin-top: -8px;
+	}
+</style>
