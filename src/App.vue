d35301339c53e82158e79598241ce8f22e5bc49c
haemory <parkha45@naver.com>
2022-02-08 23:09:40 +0900

    업로드 페이지 띄우기, 업로드한 파일 이미지 데이터 바인딩

--- src/App.vue
+++ src/App.vue
@@ -9,13 +9,14 @@
 		<img src="./assets/logo.png" class="logo" />
 	</div>
 
-	<Container :UserData="UserData" />
+
+	<Container :UserData="UserData" :container_step="container_step" :upload_img="upload_img" />
 
 	<button @click="more">더보기</button>
 
 	<div class="footer">
 		<ul class="footer-button-plus">
-		<input type="file" id="file" class="inputfile" />
+		<input @change="upload" type="file" id="file" class="inputfile" />
 		<label for="file" class="input-plus">+</label>
 		</ul>
 	</div>
@@ -34,7 +35,8 @@ export default {
 		return {
 			UserData: UserData,
 			more_count: 0,
-			tab_step: 0
+			container_step: 0,
+			upload_img: '',
 		}
 	},
 	components: {
@@ -47,6 +49,15 @@ export default {
 				this.UserData.push(result.data)
 				this.more_count++
 			})
+		},
+		upload(e) {
+			var file = e.target.files
+			let url = URL.createObjectURL(file[0])
+
+			if( file != '' ) {
+				this.container_step = 1
+				this.upload_img = url
+			}
 		}
 	},
 }
@@ -117,6 +128,48 @@ export default {
 	}
 	.input-plus {
 		cursor: pointer;
+	}
+	.upload-image{
+		width: 100%;
+		height: 450px;
+		background: cornflowerblue;
+		background-size : cover;
+	}
+	.filters{
+		overflow-x:scroll;
+		white-space: nowrap;
+	}
+	.filter-1 {
+		width: 100px;
+		height: 100px;
+		background-color: cornflowerblue;
+		margin: 10px 10px 10px auto;
+		padding: 8px;
+		display: inline-block;
+		color : white;
+		background-size: cover;
+	}
+	.filters::-webkit-scrollbar {
+		height: 5px;
+	}
+	.filters::-webkit-scrollbar-track {
+		background: #f1f1f1;
+	}
+	.filters::-webkit-scrollbar-thumb {
+		background: #888;
+		border-radius: 5px;
+	}
+	.filters::-webkit-scrollbar-thumb:hover {
+		background: #555;
+	}
+	.write-box {
+		border: none;
+		width: 90%;
+		height: 100px;
+		padding: 15px;
+		margin: auto;
+		display: block;
+		outline: none;
 	}
 
 </style>
--- src/components/container.vue
+++ src/components/container.vue
@@ -1,6 +1,26 @@
 <template>
   <div>
-		<Post v-for="(user, i) in UserData" :key="i" :user="user" />
+		<div class="post-wrap" v-if="container_step == 0">
+			<Post v-for="(user, i) in UserData" :key="i" :user="user" />
+		</div>
+
+		<div class="filter-wrap" v-if="container_step == 1">
+			<div class="upload-image" :style="{'background-image': 'url(' + upload_img + ')'}"></div>
+			<div class="filters">
+				<div class="filter-1" :style="{'background-image': 'url(' + upload_img + ')'}"></div>
+				<div class="filter-1" :style="{'background-image': 'url(' + upload_img + ')'}"></div>
+				<div class="filter-1" :style="{'background-image': 'url(' + upload_img + ')'}"></div>
+				<div class="filter-1" :style="{'background-image': 'url(' + upload_img + ')'}"></div>
+				<div class="filter-1" :style="{'background-image': 'url(' + upload_img + ')'}"></div>
+			</div>
+		</div>
+
+		<div class="write-wrap" v-if="container_step == 2">
+			<div class="upload-image" :style="{'background-image': 'url(' + upload_img + ')'}"></div>
+			<div class="write">
+				<textarea class="write-box">write!</textarea>
+			</div>
+		</div>
   </div>
 </template>
 
@@ -14,6 +34,8 @@ export default {
 	},
 	props: {
 		UserData: Array,
+		container_step: Number,
+		upload_img: File
 	}
 }
 </script>
